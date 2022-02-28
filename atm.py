967c0f92eb8a383ab4edb41b52b15d233654b34c
Nuisance256 <92674122+Yoylecake256@users.noreply.github.com>
2022-02-28 15:05:34 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,27 @@
+class Atm(object):
+    def __init__(self, name, age, cardnum, pin, ):
+        self.name = name
+        self.age = age
+        self.cardnum = cardnum
+        self.pin = pin
+    def moneyout(self, moneyOut):
+        self.moneyOut=moneyOut
+        a = 200
+        b = 200
+#Now we do the Magic bit. Now Now, you don't need to mess it up or else it will malfunction
+print("Hello I am Python AI, How Can I help you?")
+print("User: Hello Python AI!")
+print("Python AI: Can you please tell me your name?")
+input("User: ")
+print("please type your 4-digit pin code below, don't worry it will be safe and sound.")
+input("User: ")
+print("Python AI: Thank you.")
+#Please copy this: Can I check my balance bank balance?
+input("User: ")
+print("Python AI: Your current balance is ___.___$")
+input("User: ")
+print("Python AI: You are welcome")
+input("Python AI: Enter the ammount of money you want to withdraw: ")
+print("Python AI: The money you wanted is now going to come out from the box please take it before you leave.")
+print("Good Bye user")
+input("User: ")
